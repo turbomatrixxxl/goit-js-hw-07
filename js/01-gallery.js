c26f1af97d1293c5c52404bf1372d117be5a9ac4
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-04-10 22:54:14 +0300

    finished task 2 :)))

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,11 +3,13 @@ import { galleryItems } from "./gallery-items.js";
 
 const gallery = document.querySelector(".gallery");
 
+// setting arrays for gallery attributes
 const imagesPreview = [];
 const imagesOriginal = [];
 const imagesDescription = [];
 const listImages = [];
 
+// getting the arrays contents and setting the html array  with the other array content
 for (let i = 0; i < galleryItems.length; i++) {
   const element = galleryItems[i];
   imagesPreview.push(galleryItems[i].preview);
@@ -15,12 +17,13 @@ for (let i = 0; i < galleryItems.length; i++) {
   imagesDescription.push(galleryItems[i].description);
   const image = `<li class="gallery__item"><a class="gallery__link" href="${imagesOriginal[i]}"><img class="gallery__image" data-source="${imagesOriginal[i]}" src="${imagesPreview[i]}" alt="${imagesDescription[i]}"></a></li>`;
   listImages.push(image);
-  //   return listImages;
 }
 
+// putting the html array in html on broser page
 console.log(listImages);
 gallery.innerHTML = listImages;
 
+// getting the li and link arrays by class css selector
 // const galleryItem = document.querySelectorAll(".gallery__item");
 // console.log(galleryItem);
 const link = document.querySelectorAll(".gallery__link");
@@ -29,7 +32,10 @@ console.log(link);
 for (let index = 0; index < link.length; index++) {
   const element = link[index];
   element.addEventListener("click", (e) => {
+    // preventing link to engage
     e.preventDefault();
+
+    // changing image src value on click event
     const elementImage = element.querySelector("img");
     console.log(elementImage);
 
@@ -37,11 +43,13 @@ for (let index = 0; index < link.length; index++) {
     elementImage.src = elementImageAttribute;
     console.log(elementImage);
 
+    // creating the modal window with te html image inside using basicLightbox library
     const instance = basicLightbox.create(
       `<img class="gallery__image" src="${elementImage.src}" alt="${elementImage.alt}">`
     );
     instance.show();
 
+    // setting the "Esc" key to close the modal window
     document.addEventListener("keyup", (ev) => {
       if (ev.key === "Escape") {
         instance.close();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+// setting arrays for gallery attributes
+const imagesPreview = [];
+const imagesOriginal = [];
+const imagesDescription = [];
+const listImages = [];
+
+// getting the arrays contents and setting the html array  with the other array content
+for (let i = 0; i < galleryItems.length; i++) {
+  const element = galleryItems[i];
+  imagesPreview.push(galleryItems[i].preview);
+  imagesOriginal.push(galleryItems[i].original);
+  imagesDescription.push(galleryItems[i].description);
+  const image = `<li class="gallery__item"><a class="gallery__link" href="${imagesOriginal[i]}"><img class="gallery__image" data-source="${imagesOriginal[i]}" src="${imagesPreview[i]}" alt="${imagesDescription[i]}"></a></li>`;
+  listImages.push(image);
+}
+
+// putting the html array in html on broser page
+gallery.innerHTML = listImages;
+
+// getting the gallery"link" array by class css selector
+const link = document.querySelectorAll(".gallery__link");
+
+// getting each link and adding click event on each link
+link.forEach((element) => {
+  element.addEventListener("click", (ev) => {
+    //   preventing link natural action
+    ev.preventDefault();
+    const elementImage = element.querySelector("img");
+
+    // changing the src image path on click event
+    elementImage.src = element.href;
+
+    // setting the modal window gallery using the SimpleLightbox library and adding "alt" caption title on bottom with 250 ms delay
+    let gallery = new SimpleLightbox(`.gallery a`, {
+      captionsData: "alt",
+      captionDelay: 250,
+    });
+  });
+});
